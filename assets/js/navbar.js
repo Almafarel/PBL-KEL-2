50593847a185127ae7e968232788bdc84f50cbcd
Fina Ismatus Saniyah <89373176+FinaIsma@users.noreply.github.com>
2025-11-23 16:05:02 +0700

    Add files via upload

---
+++ assets/js/navbar.js
@@ -0,0 +1,16 @@
+document.addEventListener("DOMContentLoaded", () => {
+    fetch("navbar.html")
+        .then(response => response.text())
+        .then(data => {
+            document.getElementById("navbar-placeholder").innerHTML = data;
+
+            const currentPage = window.location.pathname.split("/").pop();
+            const links = document.querySelectorAll(".nav-menu a");
+
+            links.forEach(link => {
+                if (link.getAttribute("href") === currentPage) {
+                    link.classList.add("active");
+                }
+            });
+        });
+});
